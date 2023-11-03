aeec0dc68f2b291ef67baf260b9052b6de8c49aa
enya <salmaislam489265@gmail.com>
2023-11-03 10:16:01 +0600

    3rd section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
-tailwind.config = {
-    theme: {
-      extend: {
-        colors: {
-          clifford: '#da373d',
-        }
-      }
-    }
-  }
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
